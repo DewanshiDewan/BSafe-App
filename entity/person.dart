98c51fb3ad9828653643a1a981c39cbf92369444
Dewanshi Dewan <Dewanshi.Dewan@iiitb.ac.in>
2022-11-05 01:19:32 +0530

    Add files via upload

---
+++ entity/person.dart
@@ -0,0 +1,9 @@
+// import 'package:floor/floor.dart';
+//
+// @entity
+// class Person{
+//   late final int uid;
+//   late final String name;
+//
+//   Person(this.uid, this. name);
+// }
---
+++ entity/question.dart
@@ -0,0 +1,9 @@
+// import 'package:floor/floor.dart';
+//
+// @entity
+// class Question{
+//   late final int qid;
+//   late final String question;
+//
+//   Question(this.question);
+// }
