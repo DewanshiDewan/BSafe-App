60d7e585dbafffba1f7b0b5ecadf2eb931646265
Dewanshi Dewan <Dewanshi.Dewan@iiitb.ac.in>
2022-11-09 01:56:43 +0530

    Delete entity directory

--- entity/person.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:floor/floor.dart';
-//
-// @entity
-// class Person{
-//   late final int uid;
-//   late final String name;
-//
-//   Person(this.uid, this. name);
-// }
--- entity/question.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:floor/floor.dart';
-//
-// @entity
-// class Question{
-//   late final int qid;
-//   late final String question;
-//
-//   Question(this.question);
-// }
